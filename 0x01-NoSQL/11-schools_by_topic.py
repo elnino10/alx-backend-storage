737e75f59aaa929090d90befc6c9b9dc9daf977c
Joe <joeegboka@gmail.com>
2024-05-14 21:49:05 +0100

    12-log_stats.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+''' 11-schools_by_topic.py '''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Returns the list of school by topic
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
+
