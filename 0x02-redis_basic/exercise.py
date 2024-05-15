aa8e30aea624d7b5ad2a03801d61c5d580b90c14
Joe <joeegboka@gmail.com>
2024-05-16 00:04:07 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,21 @@ class Cache:
         key = f"{uuid.uuid4()}"
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float]:
+        """get method"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+
+    def get_str(self, key: str) -> str:
+        """get_str method"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+
+    def get_int(self, key: str) -> int:
+        """get_int method"""
+        return self.get(key, int)
